5a9f8b0b990f5ab30a37cf1be4d7184202873b33
chulyeon <guinness987@gmail.com>
2022-12-26 09:57:27 +0900

    폴더 추가

--- My-Bankey/Login/LoginViewController.swift
+++ My-Bankey/Login/LoginViewController.swift
@@ -12,7 +12,17 @@ class LoginViewController: UIViewController {
 		btn.addTarget(self, action: #selector(onClickSigninButton), for: .touchUpInside)
 		return btn
 	}()
-
+	private let errorMessage: UILabel = {
+		let msg = UILabel()
+		msg.translatesAutoresizingMaskIntoConstraints = false
+		msg.textAlignment = .center
+		msg.textColor = .systemRed
+		msg.numberOfLines = 0
+		msg.isHidden = true
+		msg.text = "Wrong username or password!"
+		return msg
+	}()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
@@ -23,6 +33,7 @@ class LoginViewController: UIViewController {
 	//MARK: - Selectors
 	@objc func onClickSigninButton() {
 		print("hello")
+		errorMessage.isHidden = false
 	}
 }
 
@@ -32,6 +43,7 @@ extension LoginViewController {
 		view.translatesAutoresizingMaskIntoConstraints = false
 	}
 	private func layout() {
+		// input field
 		view.addSubview(loginView)
 		NSLayoutConstraint.activate([
 			loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
@@ -39,12 +51,20 @@ extension LoginViewController {
 			view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
 		])
 		
+		// login button
 		view.addSubview(loginButton)
 		NSLayoutConstraint.activate([
-			loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			loginButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
+			loginButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
 			loginButton.topAnchor.constraint(equalTo: loginView.bottomAnchor, constant: 20),
 			loginButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
 			view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginButton.trailingAnchor, multiplier: 1)
+		])
+		
+		view.addSubview(errorMessage)
+		NSLayoutConstraint.activate([
+			errorMessage.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 20),
+			errorMessage.centerXAnchor.constraint(equalTo: loginButton.centerXAnchor)
 		])
 	}
 }
